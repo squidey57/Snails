5480c74864950747dbaa8c5cddca631655210fd7
Unknown <squidey57@gmail.com>
2018-11-13 00:31:36 +0000

    Xbox Controls and Split Screen
    -Player 1 can now be controlled by an xbox control; however, they do not rotate with the right stick quite yet.
    -There are now 4 cameras that will follow each character as they move around the map.
    
    -----------------------------------------------------------------------------
    Next to do:
    
    -Get characters to rotate.
    -2 other traps to work

--- THIS DAMN SNAIL GAME/Assets/Script_Snail/ExplorerController.cs
+++ THIS DAMN SNAIL GAME/Assets/Script_Snail/ExplorerController.cs
@@ -1,38 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ExplorerController: MonoBehaviour
 {
     private Rigidbody2D _rbody;
     private float _hAxis;
     private float _vAxis;
+    private float _rAxis;
     private float _sExplorer = 500f;
+    private float _rExplorer = 200f;
+   
+    
 
     void Start()
     {
         _rbody = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        facemouse();
-    }
-
-    void facemouse()
-    {
-        Vector2 mousePoistion = Input.mousePosition;
-        mousePoistion = Camera.main.ScreenToWorldPoint(mousePoistion);
-        Vector2 direction = new Vector2(mousePoistion.x-transform.position.x,
-            mousePoistion.y-transform.position.y);
-        transform.up = direction;
-    }
+   
     void FixedUpdate()
     {
         _hAxis = Input.GetAxis("Horizontal1");
         _vAxis = Input.GetAxis("Vertical1");
+        _rAxis = Input.GetAxis("Rotate1");
         _rbody.velocity = new Vector2(_hAxis * Time.fixedDeltaTime * _sExplorer,
             _vAxis * Time.fixedDeltaTime * _sExplorer);
+        _rbody.MoveRotation(_rbody.rotation + _rExplorer*_rAxis* Time.fixedDeltaTime);
+
     }
 
 }
---
+++ THIS DAMN SNAIL GAME/Assets/Script_Snail/camera0follow.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class camera0follow : MonoBehaviour
+{
+
+    public bool Lerp, Move; // Used for toggling different modes not needed in actual game.
+    private GameObject _target; // target for the camera to follow.
+
+    private float _cameraLerpSpeed = 20f, _cameraMoveSpeed = 500f; // Camera movement speeds.
+
+    // Use this for initialization_bLse
+    void Start()
+    {
+        // Find the object with the tag Player in the hierarchy.
+        _target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Lerp && !Move)
+        {
+            // Traditional set the camera to the players position with the offset -10 so the camera stays in the same deapth
+            transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y, -10);
+        }
+    }
+
+    // FixedUpdate is called once per physics step
+    void FixedUpdate()
+    {
+        if (Lerp)
+        {
+            // Camera Lerping movement
+            Vector3 newCameraPosition = Vector3.Lerp(transform.position, _target.transform.position,
+                Time.fixedDeltaTime * _cameraLerpSpeed);
+            transform.position = new Vector3(newCameraPosition.x, newCameraPosition.y, -10);
+        }
+        else if (Move)
+        {
+            // Camera smooth movement
+            Vector3 newCameraPosition = Vector3.MoveTowards(transform.position, _target.transform.position,
+                Time.fixedDeltaTime * _cameraMoveSpeed);
+            transform.position = new Vector3(newCameraPosition.x, newCameraPosition.y, -10);
+        }
+    }
+
+}
